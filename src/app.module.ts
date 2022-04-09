a14b4209553eb6230bb555d6e10a94d2af146728
Jakub <kubasurdej@gmail.com>
2022-04-09 23:46:08 +0200

    Make all endpoints private by default

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,11 @@ import { AmqpModule } from './amqp/amqp.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CryptoModule } from './crypto/crypto.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [AppConfigModule, HealthModule, DbModule, AmqpModule, UsersModule, AuthModule, CryptoModule],
+  providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
