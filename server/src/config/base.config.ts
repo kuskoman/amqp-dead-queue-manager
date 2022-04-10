8a4003a75944912991782e984933d52878a6df86
Jakub <kubasurdej@gmail.com>
2022-04-10 17:22:32 +0200

    Change default server port to 4100

--- server/src/config/base.config.ts
+++ server/src/config/base.config.ts
@@ -4,7 +4,7 @@ import { requireEnv } from '@src/utils/common.utils';
 export const BASE_CONFIG_KEY = 'BASE_CONFIG_KEY';
 
 export const baseConfig = registerAs(BASE_CONFIG_KEY, () => ({
-  port: +(process.env.PORT || 3000),
+  port: +(process.env.PORT || 4100),
   jwtSecret: requireEnv('JWT_SECRET'),
 }));
 
