614c15813da4851fa205ce6cbf94b6eaa93a2425
Jakub <kubasurdej@gmail.com>
2022-04-10 18:27:23 +0200

    Start creating messages component

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './auth.service';
 import { JwtInterceptor } from './jwt.interceptor';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, HttpClientModule],
-  providers: [AuthService, JwtInterceptor],
+  providers: [
+    AuthService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
 })
 export class AuthModule {}
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,15 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Message } from './messages.interfaces';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.sass']
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
+  private messagesSubject: BehaviorSubject<Message[]>;
+  public messages: Observable<Message[]>;
+  public readonly displayedColumns = ['id', 'content'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly http: HttpClient) {
+    this.messagesSubject = new BehaviorSubject<Message[]>([]);
+    this.messages = this.messagesSubject.asObservable();
   }
 
+  ngOnInit(): void {
+    this.http
+      .get<Message[]>(`${environment.apiUrl}/messages`)
+      .pipe(
+        map((messages) => {
+          this.messagesSubject.next(messages);
+        })
+      )
+      .subscribe();
+  }
 }
