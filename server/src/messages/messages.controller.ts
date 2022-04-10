84dfad1bbef6394bb10a7b60eca998d2a1412499
Jakub <kubasurdej@gmail.com>
2022-04-10 18:11:56 +0200

    Add logging for execution time for getting messages

--- server/src/messages/messages.controller.ts
+++ server/src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Controller, Get, Logger, NotFoundException, Param } from '@nestjs/common';
 import { ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { UnauthorizedErrorResponse } from '@src/utils/http/UnauthorizedResponse';
 import { GetManyMessagesDto } from './dto/getManyMessages.dto';
@@ -9,6 +9,7 @@ import { MessagesService } from './messages.service';
 @ApiUnauthorizedResponse({ type: UnauthorizedErrorResponse })
 @Controller('messages')
 export class MessagesController {
+  private readonly logger = new Logger(MessagesController.name);
   constructor(private readonly messagesService: MessagesService) {}
 
   @Get(':id')
@@ -26,6 +27,12 @@ export class MessagesController {
   @Get()
   @ApiOkResponse({ type: [MessageResponse] })
   public async findMany(@Param() query: GetManyMessagesDto) {
-    return await this.messagesService.findMany({ offset: query.offset || 0, limit: query.limit || 20 });
+    const executionStart = process.hrtime.bigint();
+
+    const messages = await this.messagesService.findMany({ offset: query.offset || 0, limit: query.limit || 20 });
+
+    const executionEnd = process.hrtime.bigint();
+    this.logger.debug(`Getting messages execution time: ${(executionEnd - executionStart) / BigInt(1e6)}ms`);
+    return messages;
   }
 }
